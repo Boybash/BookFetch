98e5f4caa27e340f8f3b7fecef55ab883a39e054
Boybash <toheebibidapo@gmail.com>
2025-08-27 22:05:57 +0100

    Project Updated

--- src/Component/Product/product.jsx
+++ src/Component/Product/product.jsx
@@ -4,15 +4,24 @@ import styles from "./style.module.css";
 const Product = () => {
   let [products, setProducts] = useState([]);
   let [error, setError] = useState("");
+  let [inputValue, setInputValue] = useState("");
+  let [query, setQuerry] = useState("chinua achebe");
 
-  const API =
-    "https://www.googleapis.com/books/v1/volumes?q=%22chinua%20achebe%22";
+  function handleSubmit(e) {
+    e.preventDefault();
+    setQuerry(inputValue);
+
+    setInputValue("");
+  }
+
+  const API = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         let response = await fetch(API);
         let data = await response.json();
-        setProducts(data.items);
+        setProducts(data.items || []);
         console.log(data.items);
       } catch (error) {
         console.log("Error fetching Data", error);
@@ -20,10 +29,28 @@ const Product = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <>
+      <section className={styles.headerContainer}>
+        <header>
+          Book<span>Room</span>
+        </header>
+        <form onSubmit={handleSubmit} className={styles.formContainer}>
+          <input
+            type="text"
+            placeholder="Search For Books...."
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+          <button>Submit</button>
+        </form>
+        <button>Log In</button>
+        <button>Sign up</button>
+      </section>
       <section className={styles.productContainer}>
         {products.length === 0 ? (
           <p> Product Loading....</p>
@@ -31,12 +58,12 @@ const Product = () => {
           products.map((product) => (
             <div key={product.id} className={styles.productBox}>
               <div className={styles.imageContainer}>
-                <img src={product.volumeInfo.imageLinks.thumbnail} />
+                <img src={product.volumeInfo.imageLinks?.thumbnail} />
               </div>
               <div className={styles.productDetails}>
                 <h1>{product.volumeInfo.title}</h1>
                 <h1>{product.volumeInfo.subtitle}</h1>
-                <h2>Authors: {product.volumeInfo.authors}</h2>
+                <h1>Authors: {product.volumeInfo.authors}</h1>
                 <h1>Publusher:{product.volumeInfo.publisher}</h1>
                 <h1>Published Date:{product.volumeInfo.publishedDate}</h1>
                 <p>{product.volumeInfo.description}</p>
